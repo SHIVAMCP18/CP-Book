581f299a39b8294d0e32193727de7397dc6a8b0a
SHIVAM CP <141670880+SHIVAMCP18@users.noreply.github.com>
2024-03-21 18:08:49 +0530

    Add files via upload

---
+++ stl/Queue.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+	queue<string> q;
+
+	int n;
+	cin >> n;
+
+	for(int i = 0; i< n ; i++){
+		string s;
+		cin >> s;
+
+		q.push(s);
+	}
+
+	while(!q.empty()){
+		cout << q.front() << endl;
+		q.pop();
+	}
+
+}
